45e2cb39b01ff7c3ecd7f2bae560e4edb8e78636
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2022-03-28 16:13:46 +0300

    iupd

---
+++ src/views/RegisterForm.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class RegisterForm extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+}
