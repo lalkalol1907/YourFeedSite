081ff54e99e100f4f45c0a8797580dc0661bc0b4
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2022-03-29 21:38:08 +0300

    upd

--- src/views/LoginForm.jsx
+++ src/views/LoginForm.jsx
@@ -9,7 +9,8 @@ class LoginForm extends Component {
             password: '',
             incorrectPassword: false,
             incorrectLogin: false,
-            buttonIsActive: true
+            buttonIsActive: true,
+            showPassword: false
         }
         this.handleLoginChange = this.handleLoginChange.bind(this)
         this.handleLPasswordChange = this.handleLPasswordChange.bind(this)
@@ -21,9 +22,9 @@ class LoginForm extends Component {
         this.setState({ ...this.state, incorrectLogin: newProps.incorrectLogin, incorrectPassword: newProps.incorrectPassword })
     }
 
-    changeButtonState() {    
+    changeButtonState() {
         let loginIsValid = /^[0-9A-Z_-]+$/i.test(this.state.login)
-        this.setState({...this.state, buttonIsActive: this.state.password.length >= 0 && this.state.login.length > 0 && loginIsValid})
+        this.setState({ ...this.state, buttonIsActive: this.state.password.length >= 0 && this.state.login.length > 0 && loginIsValid })
     }
 
     handleSubmit(event) {
@@ -53,10 +54,10 @@ class LoginForm extends Component {
                 <input type="text" value={this.state.login} onChange={this.handleLoginChange} className={this.state.incorrectLogin ?
                     'text-lg py-2 px-4 my-1 text-gray-500 bg-red-300 border-0 border-b border-red-600 w-60 hover:bg-red-200 focus:outline-none focus:bg-red-200 focus:shadow rounded-t-lg'
                     : 'text-lg py-2 px-4 my-1 text-gray-500 bg-gray-200 border-0 border-b border-gray-700 w-60 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 focus:shadow rounded-t-lg'} placeholder='Username or email' />
-                <input type="text" value={this.state.password} onChange={this.handleLPasswordChange} className={this.state.incorrectPassword ?
+                <input type={ this.state.showPassword ? "text" : "password" } value={this.state.password} onChange={this.handleLPasswordChange} className={this.state.incorrectPassword ?
                     'text-lg py-2 px-4 my-1 text-gray-500 bg-red-300 border-0 border-b border-red-600 w-60 mb-8 hover:bg-red-200 focus:outline-none focus:bg-red-200 focus:shadow rounded-t-lg' :
                     'text-lg py-2 px-4 my-1 text-gray-500 bg-gray-200 border-0 border-b border-gray-700 w-60 mb-8 hover:bg-gray-300 focus:outline-none focus:bg-gray-300 focus:shadow rounded-t-lg'} placeholder='Password' />
-                <input type="submit" value="Log In" className='text-lg py-2 px-0 my-1 rounded-full text-white bg-fuchsia-700 w-36 border-0 hover:cursor-pointer shadow hover:shadow-md hover:shadow-fuchsia-700 shadow-fuchsia-700 disabled:hover:shadow disabled:hover:shadow-fuchsia-700' disabled={!this.state.buttonIsActive}/>
+                <input type="submit" value="Log In" className='text-lg py-2 px-0 my-1 rounded-full text-white bg-fuchsia-700 w-36 border-0 hover:cursor-pointer shadow hover:shadow-md hover:shadow-fuchsia-700 shadow-fuchsia-700 disabled:hover:shadow disabled:hover:shadow-fuchsia-700' disabled={!this.state.buttonIsActive} />
                 <div className='flex-1' />
                 <button className='hover:cursor-pointer mb-2 bg-gray-200 border-0 py-0' onClick={this.handleRegisterButton}>
                     <p className='text-lg text-gray-600 py-1 underline my-0 hover:'>Register</p>
--- src/views/PostView.jsx
+++ src/views/PostView.jsx
@@ -49,9 +49,6 @@ class PostView extends Component {
     }
 
     render() {
-        var LikeIcon = this.state.liked ? <RiHeartFill className='w-6 h-auto fill-red-500 px-5 group-hover:fill-red-400' /> : <RiHeartFill className='w-6 h-auto fill-bottom-icon-color px-5 group-hover:fill-red-400' />
-        var LikeSvgClass = this.state.liked ? "group bg-red-300 border-solid border border-red-500 rounded-full mb-2 ml-2 hover:bg-red-200 hover:border-red-400 hover:cursor-pointer" : "group bg-gray-100 border-solid border border-bottom-icon-border-color rounded-full mb-2 ml-2 hover:bg-red-100 hover:border-red-400 hover:cursor-pointer"
-
         return (
             <div className="grid items-center max-w-xl border-solid border border-gray-200 rounded-2xl mb-9">
                 <div className="flex items-center py-1 px-2.5 group hover:cursor-pointer">
@@ -68,8 +65,8 @@ class PostView extends Component {
                     <h4 className='text-sm font-normal mt-0'><strong className='text-sm pr-1'>{this.username}:</strong>{this.text}</h4>
                 </div>
                 <div className='flex items-center'>
-                    <button onClick={this.onLikePressed} className={LikeSvgClass}>
-                        {LikeIcon}
+                    <button onClick={this.onLikePressed} className={this.state.liked ? "group bg-red-300 border-solid border border-red-500 rounded-full mb-2 ml-2 hover:bg-red-200 hover:border-red-400 hover:cursor-pointer" : "group bg-gray-100 border-solid border border-bottom-icon-border-color rounded-full mb-2 ml-2 hover:bg-red-100 hover:border-red-400 hover:cursor-pointer"}>
+                        <RiHeartFill className={this.state.liked ? 'w-6 h-auto fill-red-500 px-5 group-hover:fill-red-400' : 'w-6 h-auto fill-bottom-icon-color px-5 group-hover:fill-red-400'} />
                     </button>
                     <div className='flex-1' />
                     <button onClick={this.onSendPressed} className="group bg-gray-100 border-solid border border-bottom-icon-border-color rounded-full mb-2 mr-2 hover:cursor-pointer hover:bg-fuchsia-100 hover:border-fuchsia-400">
