d4b93b756c11cded6c9ec2383da943585f35776d
Konstantin <57593173+lalkalol1907@users.noreply.github.com>
2022-04-01 15:15:02 +0300

    aboba

--- src/views/RegisterForm.jsx
+++ src/views/RegisterForm.jsx
@@ -4,13 +4,31 @@ class RegisterForm extends Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            login: '',
+            password: '',
+            passwordConfirmation: '',
+            username: '',
+            
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+    }
+
+    handleSignInButton(event) {
+        event.preventDefault()
     }
 
     render() {
         return (
-            <div>
-                
-            </div>
+            <form className="h-96 text-xl p-5 flex flex-col items-center bg-gray-200 border-0 rounded-3xl shadow-2xl mt-16 w-80" onSubmit={this.handleSubmit} >
+                <button className='hover:cursor-pointer mb-2 bg-gray-200 border-0 py-0' onClick={this.handleSignInButton}>
+                    <p className='text-lg text-gray-600 py-1 underline my-0 hover:'>SignIn</p>
+                </button>
+            </form>
         )
     }
 
